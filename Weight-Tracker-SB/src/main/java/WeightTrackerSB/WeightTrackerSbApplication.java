235e2c50d1c1d61711306e9faa182a5c7f03793b
Matt Freshwaters <mattfreshy@gmail.com>
2020-11-19 11:12:10 -0600

    Adding SpringBoot framework to project directory along with Java data models

---
+++ Weight-Tracker-SB/src/main/java/WeightTrackerSB/WeightTrackerSbApplication.java
@@ -0,0 +1,13 @@
+package WeightTrackerSB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeightTrackerSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeightTrackerSbApplication.class, args);
+	}
+
+}
---
+++ Weight-Tracker-SB/src/test/java/WeightTrackerSB/WeightTrackerSbApplicationTests.java
@@ -0,0 +1,13 @@
+package WeightTrackerSB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeightTrackerSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
