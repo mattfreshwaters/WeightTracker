779bd2eeca972c42e13777cf27bced4d762bbc08
Matt Freshwaters <mattfreshy@gmail.com>
2020-11-23 17:10:54 -0600

    Created dashboard components for goals & entries

--- weight-tracker/src/app/app-routing.module.ts
+++ weight-tracker/src/app/app-routing.module.ts
@@ -4,12 +4,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { GoalsComponent } from './goals/goals.component';
 import { UsersComponent } from './users/users.component';
 import { EntriesDashboardComponent } from './entries-dashboard/entries-dashboard.component';
+import { GoalsDashboardComponent } from './goals-dashboard/goals-dashboard.component';
+import { EntryDetailsComponent } from './entry-details/entry-details.component';
+import { GoalDetailsComponent } from './goal-details/goal-details.component';
 
 const routes: Routes =[
   {path: 'users', component: UsersComponent},
   {path: 'entries', component: EntriesComponent},
   {path: 'goals', component: GoalsComponent},
   {path: 'entriesDashboard', component: EntriesDashboardComponent},
+  {path: 'entryDetail/:id ', component: EntryDetailsComponent},
+  {path: 'goalsDashBoard', component: GoalsDashboardComponent},
+  {path: 'goalDetail/:id', component: GoalDetailsComponent},
   {path: '', redirectTo: '  /entriesDashBoard', pathMatch: 'full'}
 ];
 
--- weight-tracker/src/app/goals-dashboard/goals-dashboard.component.ts
+++ weight-tracker/src/app/goals-dashboard/goals-dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Goal } from '../goal';
+import { GoalService } from '../goal.service';
 
 @Component({
   selector: 'app-goals-dashboard',
@@ -7,9 +9,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GoalsDashboardComponent implements OnInit {
 
-  constructor() { }
+  goals: Goal[] = [];
+
+  constructor(private goalService: GoalService) { }
 
   ngOnInit(): void {
+    this.getGoals();
+  }
+
+  getGoals(): void{
+    this.goalService.getGoals().subscribe(goals=>this.goals = goals);
   }
 
 }
+
+
+
+/*
+import { Component, OnInit } from '@angular/core';
+import { Entry } from '../entry';
+import { EntryService } from '../entry.service';
+
+@Component({
+  selector: 'app-entries-dashboard',
+  templateUrl: './entries-dashboard.component.html',
+  styleUrls: ['./entries-dashboard.component.css']
+})
+export class EntriesDashboardComponent implements OnInit {
+
+  entries: Entry[] = [];
+
+  constructor(private entryService: EntryService) { }
+
+  ngOnInit(): void {
+    this.getEntries();
+  }
+
+  getEntries(): void{
+    this.entryService.getEntries()
+    .subscribe(entries => this.entries = entries);
+  }
+
+}
+
+*/
